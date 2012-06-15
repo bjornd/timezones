36185b9c2e84af7747123797eca243b1304aa4dd
Kirill Lebedev <echo.bjornd@gmail.com>
2012-06-15 07:34:15 +0300

    Fixed DST

--- process.rb
+++ process.rb
@@ -7,12 +7,9 @@ require 'net/http'
 
 timezones = {}
 CSV.foreach('tz.csv', col_sep: ",", encoding: "ISO-8859-1") do |row|
-  timezones[row[2]] ||= []
-  timezones[row[2]] << row[0]
-end
-
-timezones.each_key do |k|
-  puts k
+  offset = row[2].sub('_', '-')
+  timezones[offset] ||= []
+  timezones[offset] << row[0]
 end
 
 File.open("tz-group.json", "w") do |file|
